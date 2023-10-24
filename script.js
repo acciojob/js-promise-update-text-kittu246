c536074ae30705d46804bf99884290ccb7170b5f
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-10-24 16:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function createPromise(){
+   return new Promise((resolve) => 
+     
+      setTimeout(() => {
+         resolve("Hello, world!")
+      },1000))
+}
+
+let displayDiv = document.getElementById("output");
+createPromise()
+.then((result) => displayDiv.innerText= result )
+
+.catch((err) => console.log(err));
